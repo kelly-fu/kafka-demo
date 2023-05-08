e1cba108e5869f624d235ddad052c3ecb8e69e0e
kelly <fukun15069119693@163.com>
2023-05-08 11:59:55 +0800

    first commit

---
+++ src/test/java/com/ark/cloud/kafkademo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ark.cloud.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
