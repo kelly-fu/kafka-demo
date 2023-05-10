8d7bae4bd0df7f06203ba8655979e8a4ae320c56
kelly <fukun15069119693@163.com>
2023-05-10 19:57:17 +0800

    kafka消费者代码

--- src/main/java/com/ark/cloud/kafkademo/controller/KafkaProducerController.java
+++ src/main/java/com/ark/cloud/kafkademo/controller/KafkaProducerController.java
@@ -14,15 +14,40 @@ public class KafkaProducerController {
 	KafkaTemplate kafkaTemplate;
 
 
-	@GetMapping("/send")
-	public void test() {
-		for (int i = 0; i < 10; i++) {
-			kafkaTemplate.send("es", 0, "key", String.valueOf(i));	
-		}
+	@GetMapping("/sendZero")
+	public void sendZero() {
+		//for (int i = 0; i < 10; i++) {
+		//	kafkaTemplate.send("es", 0, "key", String.valueOf(i));
+		//}
+		kafkaTemplate.send("es", 0, "key", String.valueOf("分片0是我"));
 
-		for (int i = 0; i < 5; i++) {
-			kafkaTemplate.send("es", 2, "key", "星期" + i);	
-		}
+		//for (int i = 0; i < 5; i++) {
+		//	kafkaTemplate.send("es", 2, "key", "星期" + i);	
+		//}
+	}
+
+	@GetMapping("/sendTwo")
+	public void sendTwo() {
+		//for (int i = 0; i < 10; i++) {
+		//	kafkaTemplate.send("es", 0, "key", String.valueOf(i));
+		//}
+		kafkaTemplate.send("es", 2, "key", String.valueOf("我是分片2"));
+
+		//for (int i = 0; i < 5; i++) {
+		//	kafkaTemplate.send("es", 2, "key", "星期" + i);	
+		//}
+	}
+
+	@GetMapping("/sendOne")
+	public void sendOne() {
+		//for (int i = 0; i < 10; i++) {
+		//	kafkaTemplate.send("es", 0, "key", String.valueOf(i));
+		//}
+		kafkaTemplate.send("es", 1, "key", String.valueOf("我是分片1"));
+
+		//for (int i = 0; i < 5; i++) {
+		//	kafkaTemplate.send("es", 2, "key", "星期" + i);	
+		//}
 	}
 
 
--- src/main/java/com/ark/cloud/kafkademo/kafka/KafkaConsumerServer.java
+++ src/main/java/com/ark/cloud/kafkademo/kafka/KafkaConsumerServer.java
@@ -1,11 +1,35 @@
 package com.ark.cloud.kafkademo.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * kafka消费者
  */
+
 @Component
 public class KafkaConsumerServer {
 	
+	@KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topicPartitions = {@TopicPartition(topic = "${spring.kafka.template.es-topic}", partitions = "2")})
+	private void listenPartitionTwoEsTopic(String record) {
+		System.out.println("接收到分片2需要写入ES的消息: " + record);
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topicPartitions = {@TopicPartition(topic = "${spring.kafka.template.es-topic}", partitions = "0")})
+	private void listenPartitionZeroEsTopic(String record) {
+		System.out.println("接收到分片0需要写入ES的消息: " + record);
+	}
+
+
+
 }
