2e5dcf4511927bf685c531ab3f786c144361c31f
kelly <fukun15069119693@163.com>
2023-05-11 16:27:35 +0800

    kafka删除部分无用代码

--- src/main/java/com/ark/cloud/kafkademo/kafka/KafkaConsumerServer.java
+++ src/main/java/com/ark/cloud/kafkademo/kafka/KafkaConsumerServer.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
-
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -17,12 +16,6 @@ public class KafkaConsumerServer {
 	@KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topicPartitions = {@TopicPartition(topic = "${spring.kafka.template.es-topic}", partitions = "2")})
 	private void listenPartitionTwoEsTopic(String record) {
 		System.out.println("接收到分片2需要写入ES的消息: " + record);
-		try {
-			Thread.sleep(20000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 	@KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topicPartitions = {@TopicPartition(topic = "${spring.kafka.template.es-topic}", partitions = "0")})
